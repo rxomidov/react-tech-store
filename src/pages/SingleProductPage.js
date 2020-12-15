278ea5734c74c866d3715cfaa252e4110b8bd9ce
Rixsitillo <rixsitilloxomidov@gmail.com>
2020-12-15 13:16:47 +0500

    contentful  commit1

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import spBg from '../images/singleProductBcg.jpeg'
+import spBg from '../images/singleProductBcg.jpeg';
 import {ProductConsumer} from "../context";
 import Hero from "../components/Hero";
 import {Link} from "react-router-dom";
